54d8c38cc0e90542c37cad748355db1cb20e02a8
Stteevn <stteevntang@gmail.com>
2019-06-08 17:54:35 +0800

    commit

---
+++ lab9/replace.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+int K, alg, n;
+
+int main() {
+    scanf("%d", &K);
+    scanf("%d", &alg);
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+
+    }
+    return 0;
+}
